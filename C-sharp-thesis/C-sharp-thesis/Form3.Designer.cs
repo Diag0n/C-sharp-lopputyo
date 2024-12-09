32bcf1402179b6c95e99c54336b2bc22e52e9d26
Julius Tikkanen <Julius.Tikkanen@gmail.com>
2024-12-09 12:56:11 +0200

    Pelin toiminnallisuus sai alkunsa

--- C-sharp-thesis/C-sharp-thesis/Form3.Designer.cs
+++ C-sharp-thesis/C-sharp-thesis/Form3.Designer.cs
@@ -1,4 +1,7 @@
 ﻿
+using System;
+using System.Windows.Forms;
+
 namespace C_sharp_thesis
 {
     partial class Form3
@@ -30,8 +33,6 @@ namespace C_sharp_thesis
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -46,32 +47,33 @@ namespace C_sharp_thesis
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(37, 46);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(540, 689);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(684, 911);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form3";
             this.Text = "Form3";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("PictureBoxia klikattiin!");
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- C-sharp-thesis/C-sharp-thesis/Form3.cs
+++ C-sharp-thesis/C-sharp-thesis/Form3.cs
@@ -1,25 +1,160 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace C_sharp_thesis
 {
     public partial class Form3 : Form
     {
+        private TetrisGame tetrisGame;
+        private PictureBox pictureBox1;
+
         public Form3()
         {
             InitializeComponent();
+            KeyPreview = true;
+            InitializeGameUI();
+            tetrisGame = new TetrisGame(pictureBox1);
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void InitializeGameUI()
+        {
+            pictureBox1 = new PictureBox
+            {
+                Location = new Point(10, 10),
+                Size = new Size(200, 400),
+                BorderStyle = BorderStyle.FixedSingle
+            };
+
+            button1 = new Button
+            {
+                Text = "Palaa",
+                Location = new Point(220, 10),
+                Size = new Size(80, 30)
+            };
+
+            button1.Click += Button1_Click;
+
+            Controls.Add(pictureBox1);
+            Controls.Add(button1);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+            tetrisGame.HandleKeyDown(e.KeyCode);
+        }
+    }
+
+    public class TetrisGame
+    {
+        private readonly int tileSize = 20;
+        private int[,] currentPiece;
+        private Point piecePosition;
+        private readonly PictureBox pictureBox;
+        private readonly Bitmap gameBitmap;
+
+        private readonly int[,] IBlock = new int[4, 4]
+        {
+        { 0, 0, 0, 0 },
+        { 1, 1, 1, 1 },
+        { 0, 0, 0, 0 },
+        { 0, 0, 0, 0 }
+        };
+
+        public TetrisGame(PictureBox pictureBox)
+        {
+            this.pictureBox = pictureBox;
+            this.gameBitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
+            InitGame();
+        }
+
+        private void InitGame()
+        {
+            currentPiece = IBlock;
+            piecePosition = new Point(3, 0);
+            Timer timer = new Timer { Interval = 500 };
+            timer.Tick += (s, e) => MovePiece(0, 1);
+            timer.Start();
+            DrawGame();
+        }
+
+        public void HandleKeyDown(Keys keyCode)
+        {
+            switch (keyCode)
+            {
+                case Keys.Left:
+                    MovePiece(-1, 0);
+                    break;
+                case Keys.Right:
+                    MovePiece(1, 0);
+                    break;
+                case Keys.Down:
+                    MovePiece(0, 1);
+                    break;
+                case Keys.Up:
+                    RotatePiece();
+                    break;
+            }
+        }
+
+        private void MovePiece(int dx, int dy)
+        {
+            piecePosition.X += dx;
+            piecePosition.Y += dy;
+            DrawGame();
+        }
+
+        private void RotatePiece()
+        {
+            int size = currentPiece.GetLength(0);
+            int[,] rotatedPiece = new int[size, size];
+
+            for (int y = 0; y < size; y++)
+            {
+                for (int x = 0; x < size; x++)
+                {
+                    rotatedPiece[x, size - 1 - y] = currentPiece[y, x];
+                }
+            }
+
+            currentPiece = rotatedPiece;
+            DrawGame();
+        }
+
+
+        private void DrawGame()
+        {
+            using (Graphics g = Graphics.FromImage(gameBitmap))
+            {
+                g.Clear(Color.Black);
+
+                for (int y = 0; y < currentPiece.GetLength(0); y++)
+                {
+                    for (int x = 0; x < currentPiece.GetLength(1); x++)
+                    {
+                        if (currentPiece[y, x] == 1)
+                        {
+                            g.FillRectangle(Brushes.Cyan,
+                                (piecePosition.X + x) * tileSize,
+                                (piecePosition.Y + y) * tileSize,
+                                tileSize, tileSize);
+                            g.DrawRectangle(Pens.Black,
+                                (piecePosition.X + x) * tileSize,
+                                (piecePosition.Y + y) * tileSize,
+                                tileSize, tileSize);
+                        }
+                    }
+                }
+            }
+
+
+            pictureBox.Image = (Image)gameBitmap.Clone();
         }
     }
 }
